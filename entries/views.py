2b7e232e41e566a4162f37c13ea327198b72ddf1
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2020-06-07 14:40:53 -0700

    created ListView

--- entries/views.py
+++ entries/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Entry
+from django.views.generic import ListView
 # Create your views here.
+
+class HomeView(ListView):
+    model = Entry
+    template_name = 'entries/index.html'
+    context_object_name = 'entries'
