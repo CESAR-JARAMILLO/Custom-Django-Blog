95e7f74a6e471f7c1c29145690d8576dcc4e48d6
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2020-06-08 20:06:00 -0700

    created DetailView

--- entries/views.py
+++ entries/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from .models import Entry
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 # Create your views here.
 
 class HomeView(ListView):
     model = Entry
     template_name = 'entries/index.html'
     context_object_name = 'entries'
+
+class EntryView(DetailView):
+    model = Entry
+    template_name = 'entries/entry_detail.html'
